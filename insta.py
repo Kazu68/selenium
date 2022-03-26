4829fa0a836fa18ece3b1732639e51a6b8bf1fea
Kazu68 <64633076+Kazu68@users.noreply.github.com>
2022-03-26 19:22:56 +0900

    Update insta.py
    added chromedriver

--- insta.py
+++ insta.py
@@ -1,2 +1,7 @@
 from selenium import webdriver
 from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+driver.get('https://www.google.co.jp')
